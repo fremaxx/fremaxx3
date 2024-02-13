7742fd490bf5dc2b6e53e6ecb38921447de9e86c
fremaxx <159620763+fremaxx@users.noreply.github.com>
2024-02-13 02:11:27 +0100

    Add files via upload

---
+++ models.js
@@ -0,0 +1,377 @@
+const kitchenModels={
+    kitchen: [
+        {
+            name: "Hamilton Beach 4-Quart Slow Cooker with 3 Cooking Settings, Dishwasher-Safe Stoneware Crock & Glass Lid",
+            image: "photos/kitchen1.jpg",
+            description: "The Hamilton Beach slow cooker boasts simple manual controls for effortless, flavorful meals. With a 4-quart capacity, it's perfect for one-pot dishes, accommodating a 4 lb. chicken or 2 lb. roast. Choose Low or High settings and switch to Keep Warm. The removable stoneware crock and glass lid are dishwasher-safe, and the crock is oven and microwave-safe for versatile cooking.",
+            price: 34.99,
+            amazonLink: 'https://amzn.to/3SWHFFJ'
+           
+        },
+        {
+            name: "Betty Crocker Countertop Pizza Maker, 1440-Watt Pizza Maker Machine for Home, BC-2958CR",
+            image: "photos/kitchen2.jpg",
+            description: " Betty Crocker's 12 inch pizza pan & indoor grill (13.78” x 15.87” x 5.04”) is a versatile joy for homemade pizzas. With 1440W power, it functions as a sandwich maker, griddle press, tortilla press, crepe maker, omelet maker, and quesadilla maker. Its nonstick surface ensures easy cleaning, making it perfect for oven-free, indoor cooking and creating lasting memories.",
+            price: 53.99,
+            amazonLink:'https://amzn.to/49uawGX'
+        },
+        {
+            name: "Bentgo® Classic - Adult Bento Box, All-in-One Stackable Lunch Box Container with 3 Compartments",
+            image: "photos/kitchen3.jpg",
+            description: "       The adult bento box is a versatile on-the-go meal solution with a stackable design for separating warm and cold foods, ideal for office lunches. Each compartment supports portion control, and the all-in-one box includes utensils. Compact and lightweight, it easily fits into lunch bags, accommodating a full meal. Microwave and dishwasher safe, excluding the top lid, divider, and nylon strap.",
+            price: 29.99,
+            amazonLink: 'https://amzn.to/49fUvF0'
+           
+        },
+        {
+            name: "CAROTE Nonstick Frying Pan Skillet,8 inch Non Stick Granite Fry Pan with Glass Lid, Egg Pan Omelet Pans" ,
+            image: "photos/kitchen4.jpg",
+            description: " Eco-friendly Classic Granite cookware is a safe, non-stick solution. Easy to clean with minimal environmental impact, it suits all stoves, including induction, heating evenly. Requiring less oil, it reduces smoke and is versatile for various cooking needs. CAROTE ensures quality, offering a 100% refund or replacement for any defect items caused by delivery.",
+            price: 29.99,
+            amazonLink: 'https://amzn.to/3SFcM7m'
+        },
+        {
+            name: "KOOC Small Slow Cooker, 2-Quart, Free Liners Included for Easy Clean-up, Upgraded Ceramic pot, Adjustable Temp",
+            image: "photos/kitchen5.jpg",
+            description: "            KOOC slow cooker, with 5 free liners, simplifies cleanup. Low-power operation retains food nutrients. Easy-to-use classic knob and 3-mode temperature adjustment. Available in various colors and sizes (2~8.5 Qt). Upgraded ceramic pot and stainless steel body ensure durability. Superior service with a 1-year warranty, 30-days return/exchange, and 24-hour response guarantee.",
+            price: 39.99,
+            amazonLink: 'https://amzn.to/3wh31VB'
+        },
+        {
+            name: "[NEW LAUNCH] KOOC 8.5-Quart Programmable Slow Cooker, with Digital Countdown Timer, Free Liners Included",
+            image: "photos/kitchen6.jpg",
+            description: "      KOOC slow cooker simplifies cleanup with 5 free liners and low-power operation for healthier cooking. The programmable digital countdown timer allows cook times from 30 minutes to 20 hours, shifting to Warm automatically. Available in various colors and sizes (2~8.5 Qt) with upgraded ceramic pot and stainless steel body. Superior service includes a 1-year warranty.",
+            price: 89.99,
+            amazonLink: 'https://amzn.to/4bykusK'
+        },
+        {
+            name: "DASH Dream Green Ceramic Recycled Aluminum 15 Piece Cookware Set- Pink, 	Stainless Steel, Aluminum, Ceramic",
+            image: "photos/kitchen7.jpg",
+            description: "     The Dash Dream Green Ceramic Recycled Aluminum 15pc Cookware Set, with 100% recycled aluminum bodies, ensures durability. It features forged aluminum construction, stainless steel induction plate for superior heating, and CleanCoating Safe Ceramic Nonstick coating. Oven and dishwasher safe, it includes heat-resistant silicone handles for all cooktops, including induction.",
+            price: 169.99,
+            amazonLink: 'https://amzn.to/3UHoIYX'
+           
+        },
+        {
+            name: "Nuwave Duet Electric Pressure Cooker & Air Fryer Combo, 450 IN 1 Slow Cooker & Grill with Integrated Digital Temp Probe",
+            image: "photos/kitchen8.jpg",
+            description: "The NuWave 450-in-1 Multi-Function Cooker offers versatile cooking with 3 removable lids for air fry, pressure cook, slow cook, grill, and more. 300 presets and 150 memory slots make it easy. Dynamic Pressure Control cooks 70% faster. Combo cooking delivers tenderness with 90% less oil. Triple-thick inner pot, 6QT size, and 15 safety features ensure ease and safety. Intuitive digital displa and 2-in-1 dial control.",
+            price: 189.99,
+            amazonLink: 'https://amzn.to/3uw5Hhp'
+        },
+        {
+            name: "Ultrean Food Scale, Digital Kitchen Scale Weight Grams and Ounces for Baking Cooking and Meal Prep, 6 Units ",
+            image: "photos/kitchen9.jpg",
+            description: " Cook with Color's 4-piece mixing bowl set provides non-slip, sturdy bowls with lids for easy cooking and baking. Dishwasher, freezer, and food safe, these BPA-free bowls range from 210 ml to 3500 ml. The nesting design ensures convenient storage, saving space. With a 100% customer satisfaction guarantee, Cook with Color prioritizes your experience and welcomes any questions or concerns.  ",
+            price: 19.99,
+            amazonLink: 'https://amzn.to/3UGfNHc'
+           
+        },
+        {
+            name: "Ultrean Food Scale, Digital Kitchen Scale Weight Grams and Ounces for Baking Cooking and Meal Prep, 6 Units",
+            image: "photos/kitchen10.jpg",
+            description: "   Ultrean's Stainless Steel Kitchen Scale boasts a sleek design for easy use and cleaning. The user-friendly control panel provides accurate weight data, including milk volume measurement. With low energy consumption, auto shut-off, and a backlit display, it offers convenience. Ultrean ensures a reliable experience with after-sales support, a comprehensive warranty, and a dedicated customer support team.",
+            price: 14.99,
+            amazonLink: 'https://amzn.to/3SXvKrt'
+        },
+        {
+            name: "OXO Good Grips Glass Salad Spinner, Large/6.22 Quart, Clear, Elegant yet durable",
+            image: "photos/kitchen11.jpg",
+            description: "The borosilicate glass bowl is both elegant and durable, suitable for serving or storage. With a one-handed pump for effortless drying and a patented brake for immediate stopping, this system is efficient. The storage lock facilitates stacking, and the removable spinner basket serves as a colander for rinsing greens. The easy-to-clean lid disassembles, and the entire set is dishwasher safe for convenience.",
+            price: 64.99,
+            amazonLink: 'https://amzn.to/3ODcpsX'
+           
+        },
+        {
+            name: "EKitchenAid Classic Multifunction Can Opener / Bottle Opener, 8.34-Inch, Almond Cream",
+            image: "photos/kitchen12.jpg",
+            description: "Effortlessly open cans with the durable stainless steel cutting wheel of this can opener. The large, easy-to-turn knob ensures secure grip and simple operation. Oversized, ergonomic handles provide comfort and control, with an integrated bottle opener for versatile use. Suitable for any sized can or bottle, this tool makes kitchen tasks easy and efficient.  ",
+            price: 17.70,
+            amazonLink: 'https://amzn.to/42zNkVz'
+        },
+        {
+            name: "Silicone Cooking Utensils Set - 446°F Heat Resistant Silicone Kitchen Utensils for Cooking,Kitchen Utensil Spatula Set ",
+            image: "photos/kitchen13.jpg",
+            description: "This silicone cooking utensils set is a kitchen essential with heat-resistant, non-melting wooden handles. Made from safe, BPA-free, odorless food-grade silicone, it includes 9 cooking tools, 4 baking tools, and a utensil holder. With a stainless steel core to prevent warping and easy cleaning, it's a thoughtful gift choice with rapid after-sales support.",
+            price: 32.99,
+            amazonLink: 'https://amzn.to/48fbTrV'
+        },
+        {
+            name: "40 oz Tumbler with Handle and Straw Lid, 100% Leak-proof Travel Coffee Mug, Stainless Steel Insulated Cup ",
+            image: "photos/kitchen14.jpg",
+            description: " Soufull Tumbler, with its 100% leak-proof MagSlider lid, boasts double-wall vacuum insulation for up to 34 hours of cold drinks and 10 hours of hot beverages. The 40 oz capacity, portable design, and silicone sleeve straw make it perfect for travel. Dishwasher safe, BPA-free, and odor-free, Soufull ensures 100% customer satisfaction with prompt resolution of any quality issues.",
+            price: 39.99,
+            amazonLink: 'https://amzn.to/3uAi2kL'
+           
+        },
+        {
+            name: "Zulay Kitchen Premium Garlic Press with Soft, Easy to Squeeze Handle - Includes Silicone Garlic Peeler ",
+            image: "photos/kitchen15.jpg",
+            description: "Zulay Kitchen's Garlic Press is a durable, rust-resistant tool designed for easy use and longevity. Efficiently crush and mince unpeeled cloves with its built-in studs and ergonomic handle. Easy to clean, it's versatile for crushing seeds, ginger, onions, or nuts. Backed by Zulay's lifetime warranty, it's a reliable addition to your kitchen.  ",
+            price: 19.99,
+            amazonLink: 'https://amzn.to/3SEBftp'
+        },
+        {
+            name: "THERMOS Stainless King Vacuum-Insulated Food Jar with Spoon, 16 Ounce, Army Green",
+            image: "photos/kitchen16.jpg",
+            description: "  THERMOS Food Jar keeps meals hot for 9 hours or cold for 14 hours with vacuum insulation. Made from durable 18/8 stainless steel, it's perfect for any occasion. The sweat-proof exterior stays cool with hot contents and remains dry with cold. Dishwasher-safe, it includes a folding spoon and comes in various capacities. THERMOS, since 1904, enhances everyday life, ensuring freshness in food and drinks.",
+            price: 24.99,
+            amazonLink: 'https://amzn.to/3wgSPfP'
+        },
+        {
+            name: "Kitchy Pizza Cutter Wheel with Protective Blade Cover, Ergonomic Pizza Slicer (Blue)",
+            image: "photos/kitchen17.jpg",
+            description: "Experience the effortless cutting power of the No Effort Pizza Cutter. With stable vertical pressure and a large cutting wheel, it delivers perfectly controlled slicing. Designed to fit comfortably in your hand and cutlery drawer, it disassembles for easy cleaning and is dishwasher safe. The included protective blade guard ensures safe storage.",
+            price: 19.99,
+            amazonLink: 'https://amzn.to/3UGk4dI'
+           
+        },
+        {
+            name: "KitchenClouds Kitchen Mat Cushioned Anti Fatigue Rug 17.3 inch x 28 inch Waterproof, Non Slip and Standing ",
+            image: "photos/kitchen18.jpg",
+            description: " The Comfort and Quality Kitchen Rug, made of high-quality PVC foam, provides excellent support and comfort. Its anti-slip diamond-shaped design and beveled edge enhance safety. Easy to clean, it suits any decor and is perfect for the kitchen, office, or laundry room. Ensure proper placement on a dry, flat surface for optimal use. ",
+            price: 32.99,
+            amazonLink: 'https://amzn.to/3SSke0h'
+        },
+      
+    ],
+   
+    
+
+};
+
+function updatekitchenItems(category, container) {
+    const kitchenModelsList = container || document.getElementById("kitchen-items");
+
+    kitchenModelsList.innerHTML = "";
+
+    if (category === 'all') {
+        
+        const allItems = [];
+        for (const categoryKey in kitchenModels) {
+            if (kitchenModels.hasOwnProperty(categoryKey)) {
+                allItems.push(...kitchenModels[categoryKey]);
+            }
+        }
+
+        for (let i = 0; i < allItems.length; i += 3) {
+            const row = document.createElement("div");
+            row.classList.add("kitchen-row");
+
+            for (let j = i; j < i + 3 && j < allItems.length; j++) {
+                createMenuItem(allItems[j], row);
+            }
+
+            kitchenModelsList.appendChild(row);
+        }
+    } else {
+        
+        const items = kitchenModels[category];
+
+        for (let i = 0; i < items.length; i += 3) {
+            const row = document.createElement("div");
+            row.classList.add("kitchen-row");
+
+            for (let j = i; j < i + 3 && j < items.length; j++) {
+                createMenuItem(items[j], row);
+            }
+
+            kitchenModelsList.appendChild(row);
+        }
+    }
+
+
+
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function createMenuItem(item, container) {
+    const li = document.createElement("li");
+    const name = document.createElement("span");
+    const price = document.createElement("span");
+    const addButton = document.createElement("a"); // Change to 'a' element
+    const image = document.createElement("img");
+    const description = document.createElement("p");
+
+    name.textContent = item.name;
+    price.textContent = `Price: $${item.price.toFixed(2)}`;
+
+    // Use the provided Amazon link for each button
+    addButton.href = item.amazonLink;
+    
+    addButton.textContent = "Continue";
+    addButton.setAttribute("data-name", item.name);
+    addButton.setAttribute("data-price", item.price.toFixed(2));
+
+    description.textContent = item.description;
+    image.src = item.image;
+    image.alt = item.name;
+
+    li.appendChild(name);
+    li.appendChild(image);
+    li.appendChild(description);
+    li.appendChild(price);
+    li.appendChild(addButton);
+
+    if (container) {
+        container.appendChild(li);
+    } else {
+        const row = document.createElement("div");
+        row.classList.add("kitchen-row");
+        row.appendChild(li);
+        kitchenModelsList.appendChild(row);
+    }
+}
+
+
+
+
+
+document.getElementById("all-button").click();
+
+document.addEventListener("DOMContentLoaded", function() {
+    applyPriceFiltering();
+    
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+
+    const kitchenForm = document.getElementById("kitchen-form");
+    kitchenForm.addEventListener("submit", addNewkitchen);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function removeFromkitchent(event){
+    event.target.parentElement.remove();
+ 
+    calculateTotal();
+ 
+   
+ }
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+ 
+
+ function updatekitchentItem(existingItem, itemPrice, quantity) {
+    const newPrice = itemPrice * quantity;
+    existingItem.dataset.quantity = quantity;
+    existingItem.querySelector("span:nth-child(1)").textContent = `${existingItem.dataset.name} x${quantity}`;
+    existingItem.querySelector("span:nth-child(2)").textContent = `$${newPrice.toFixed(2)}`;
+
+    calculateTotal(); 
+}
+
+function calculateTotal(){
+    const basketItems = document.querySelectorAll("#basket-items li");
+    let subtotal = 0;
+    let tax = 0;
+    let total = 0;
+    const taxRate= 0.18;
+
+    basketItems.forEach((item) =>{
+        const itemPrice = parseFloat(item.querySelector("span:nth-child(2)").textContent.slice(1));
+        subtotal += itemPrice;
+    });
+
+    tax = subtotal * taxRate;
+    total = subtotal + tax;
+
+    document.querySelector("#subtotal-price").textContent = `${subtotal.toFixed(2)}`;
+    document.querySelector("#tax-price").textContent = `${tax.toFixed(2)}`;
+    document.querySelector("#totali-price").textContent = `${total.toFixed(2)}`;
+}
+
+
+
+const kitchen = document.getElementById("kitchen");
+kitchen.addEventListener("change" , updatekitchenItems);
+
+updatekitchenItems();
+
+
+
+
+const kitchentList = document.getElementById("basket-items");
+
+kitchentList.addEventListener("click", (event) => {
+if (event.target.classList.contains("add") || event.target.classList.contains("remove")){
+calculateTotal();
+}
+});
+
+calculateTotal();
+
+
